ff324910cbd81f9952576d0aa228eba927d5a3b4
Kingsman0021 <muhammadbilal.online4@gmail.com>
2025-06-21 22:31:29 +0500

    Hero Section Updated

--- client/src/components/LenisProvider.jsx
+++ client/src/components/LenisProvider.jsx
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 const LenisProvider = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.8,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
       smoothTouch: false,
